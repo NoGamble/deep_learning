3d5fed8b7592f87472611d451cf63a938892e81e
NoGamble <uestc520@gmail.com>
2025-10-21 23:20:55 +0800

    Oct21

--- data_preprocess/tensor.py
+++ data_preprocess/tensor.py
@@ -12,4 +12,9 @@ print(X)
 print(Y)
 print(Z)
 
+# to get a random tensor
+X = torch.randn(3, 4)
+x = torch.tensor([1.0, 2, 4, 8])
+y = torch.tensor([2, 2, 2, 2])
+x + y, x - y, x * y, x / y, x ** y
 
