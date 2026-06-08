2b2dad3bd1f64185bb403f4d0072357d2a363d9b
NoGamble <uestc520@gmail.com>
2026-06-08 16:09:08 +0800

    feat: tensor

--- data_preprocess/tensor.py
+++ data_preprocess/tensor.py
@@ -1,20 +1,22 @@
 import torch
 
+# tensor basic knowledges
 x = torch.arange(12)
-# size of the tensor
+
+print(x)
+print(x.shape)
+print(x.size)
 print(x.numel())
 
-# change the shape of the tensor
 X = x.reshape(3, 4)
-Y = torch.zeros(2, 3, 4)
-Z = torch.ones((2, 3, 4))
+
 print(X)
-print(Y)
-print(Z)
+print(X.shape)
+print(X.size)
+print(X.numel())
 
-# to get a random tensor
-X = torch.randn(3, 4)
-x = torch.tensor([1.0, 2, 4, 8])
-y = torch.tensor([2, 2, 2, 2])
-x + y, x - y, x * y, x / y, x ** y
+y = torch.zeros([3, 4])
+print(y)
 
+z = torch.ones((2, 3, 4))
+print(z)
