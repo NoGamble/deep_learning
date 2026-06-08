44f480695124a007972f5ae8da80df1183a654b5
NoGamble <uestc520@gmail.com>
2026-06-08 21:00:42 +0800

    exercise: 2-1

--- data_preprocess/tensor.py
+++ data_preprocess/tensor.py
@@ -20,3 +20,39 @@ print(y)
 
 z = torch.ones((2, 3, 4))
 print(z)
+
+# operations
+print(X + y)
+
+X[0:2, :] = 1
+
+print(X)
+
+# save memory
+mem_sav = torch.zeros_like(X)
+print("id(mem_sav)", id(mem_sav))
+mem_sav[:] = X + y
+print("id(mem_sav)", id(mem_sav))
+
+# convert to other object
+A = X.numpy()
+B = torch.tensor(A)
+print(type(A), type(B))
+
+# exercise for 2.1
+a = torch.arange(3).reshape(3, 1)
+print(a)
+b = torch.arange(2).reshape(1, 2)
+print(b)
+print("a+b:", a + b)
+
+A = torch.tensor([[[1, 1, 1]], [[2, 2, 2]]])
+
+B = torch.tensor([[[1], [2], [3], [4]]])
+
+result = A + B
+
+print(result)
+
+new_tensor = torch.randn(2, 3, 4)
+print(new_tensor)
