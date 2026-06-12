88d8e6a4cffdbee78163568781764447df15bee6
NoGamble <uestc520@gmail.com>
2026-06-12 16:57:25 +0800

    feat: softmax evaluate

--- linear_classification/softmax.py
+++ linear_classification/softmax.py
@@ -2,6 +2,16 @@ import torch
 from IPython import display
 from data_loader import load_data_fashion_mnist
 
+class Accumulator:
+    def __init__(self, n):
+        self.data = [0.0] * n
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+    def __getitem__(self, idx):
+        return self.data[idx]
+
 batchsize = 256
 num_inputs = 28 * 28
 num_outputs = 10
@@ -17,6 +27,25 @@ def soft_max(X):
 def net(X):
     return soft_max(torch.matmul(X.reshape((-1, W.shape[0])), W) + b)
 
+def cross_entropy(y_hat, y):
+    return - torch.log(y_hat[range(len(y_hat)), y])
+
+def accuracy(y_hat, y):
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.astype(y.dtype) == y
+    return float(cmp.astype(y.dtype).sum())
+
+def evaluate_accuracy(net, data_iter):
+    if isinstance(net, torch.nn.Module):
+        net.eval()
+    metrics = Accumulator(2)
+    with torch.no_grad():
+        for X, y in data_iter:
+            metrics.add(accuracy(net(X), y), y.numel())
+    return metrics[0] / metrics[1]
+
+    
 
 def main():
     train_iter, test_iter = load_data_fashion_mnist(batchsize)
